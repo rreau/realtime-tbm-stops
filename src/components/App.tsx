85b72f587de926e3478690f198f4304774ffcb12
NootNook <67638224+NootNook@users.noreply.github.com>
2022-05-15 15:19:27 +0200

    Initial commit/project

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+const App = () => {
+
+  return (
+    <div className="bg-red-700 h-screen	w-screen">
+      Hello world! 
+    </div>
+  );
+}
+
+export default App;
