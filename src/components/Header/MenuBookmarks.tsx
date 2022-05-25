29a9aa94429fed79fd7610d6f5ee9d10ce98d503
NootNook <67638224+NootNook@users.noreply.github.com>
2022-05-25 20:33:58 +0200

    feat: operational bookmark menu

--- src/components/Header/MenuBookmarks.tsx
+++ src/components/Header/MenuBookmarks.tsx
@@ -1,4 +1,6 @@
+import { StopPointContext } from '@contexts/StopPointListener';
 import { getBookmarks } from '@utils/bookmarks';
+import { useCallback, useContext } from 'react';
 
 interface IData {
   line: string;
@@ -7,7 +9,19 @@ interface IData {
 }
 
 const MenuBookmarks = () => {
+  const stopPointTheme = useContext(StopPointContext);
   const elements = getBookmarks();
+
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      const id = (e.currentTarget as HTMLButtonElement).dataset.id as string;
+      const line = (e.currentTarget as HTMLButtonElement).dataset.line as string;
+      const destination = (e.currentTarget as HTMLButtonElement).dataset.destination as string;
+      const data = { id: id, line: line, destination: destination };
+      stopPointTheme?.setStopPoint((sp) => [...sp, data]);
+    },
+    [stopPointTheme]
+  );
 
   return (
     <div className='flex flex-col flex-1 max-h-[60vh] overflow-auto'>
@@ -17,6 +31,7 @@ const MenuBookmarks = () => {
           data-line={e.line}
           data-destination={e.destination}
           key={e.line + e.destination + e.id}
+          onClick={handleClick}
           className='border border-black button-primary flex flex-col'
         >
           <span className='text-left font-bold'>{e.line}</span>
